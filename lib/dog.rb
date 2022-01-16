bf7bfe833601ed6a1c11750d5a5d079f0ba3728f
Nicholas <nikhsd899@outlook.com>
2022-01-16 15:00:14 -0800

    commit tree 4f6efcee84fb91491c673e779819c61d95685553

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,20 @@
 class Dog
-    def name=(name)
-        @name = name
-    end
+    
+     # getter method
     def name
-        @name
+      @name
     end
+  
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    def breed 
+        @breed
+    end
+
     def breed=(breed)
         @breed = breed
-    end
-    def breed
-        @breed
     end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,26 @@
+
+
+
 class Person
-    def name=(name)
-        @name = name
-    end
-    def name
-        @name
-    end
-    def job=(job)
-        @job = job
-    end
-    def job
-        @job
-    end
+
+# getter method   
+def name
+    @name
+end
+
+# setter
+def name=(name)
+    @name = name
+end
+
+# getter method
+def job
+    @job
+end
+
+# setter     
+def job=(job)
+    @job = job
+end
+  
 end
